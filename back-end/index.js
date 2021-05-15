70ec6b4f45c364c2b1f7f7dbabca2d8353dd2c9d
Adan Constanzo <aconstanzolan@gmail.com>
2021-05-14 22:18:13 -0700

    Added process.env.PORT for server port.

--- back-end/index.js
+++ back-end/index.js
@@ -9,12 +9,13 @@ const people = require("./routes/people");
 const starship = require("./routes/starships");
 const film = require("./routes/films");
 
+const port = process.env.PORT || 8081;
 
 app.use(bodyParser.json());
 
 app.use((req, res, next) => {
     res.setHeader("Access-Control-Allow-Methods", "POST, PUT, OPTIONS, DELETE, GET");
-    res.header("Access-Control-Allow-Origin", "http://localhost:8081");
+    res.header("Access-Control-Allow-Origin", "http://localhost:"+port);
     res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
     res.header("Access-Control-Allow-Credentials", true);
     next();
@@ -29,7 +30,7 @@ app.use("/api/people", people);
 app.use("/api/starship", starship);
 app.use("/api/film", film);
 
-const port = 8081;
+
 
 app.listen(port, () => {
     console.log(`Running on localhost:${port}`);
